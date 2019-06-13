9fb515b3f9d55d52892aa9012f3440f9503c9aeb
Firehiwot1 <fmazengia@gmail.com>
2019-06-13 10:58:21 -0400

    Escape commit

---
+++ src/Box.java
@@ -0,0 +1,12 @@
+public class Box {
+    public static void main(String[]args) {
+        System.out.print("*******\n");
+        System.out.print("* \t " + " * \n");
+        System.out.print("* \t " + " * \n");
+        System.out.print("* \t " + " * \n");
+        System.out.print("* \t " + " * \n");
+        System.out.print("*******\n");
+
+        }
+    }
+
